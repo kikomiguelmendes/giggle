9b38f310c033c5434727bcd92651e31a7e2f48c8
kikomiguelmendes <kikomiguelmendes@gmail.com>
2025-03-29 23:44:53 +0100

    another joke button

--- script.js
+++ script.js
@@ -1,54 +1,11 @@
 let button_click_status = false;
 let jokes = [];
+let currentJokeBox = null;
 
-function button_was_clicked() {
-    button_click_status = true;
-    const button = document.getElementById('button');
-    button.remove();
-    h1.remove();
-    joke_appears();
-}
-
-function button_was_dragged() {
-    if (!button_click_status) {
-        const button = document.getElementById('button');
-        button.innerText = "Do you wanna hear a joke?";
-    } 
-}
-
-function reset_button() {
-    if (!button_click_status ) {
-        const button = document.getElementById('button');
-        button.innerText = "Don't be shy";
-        button.style.background = "";
-    }
-}
-
-function another_joke_button() {
-    const button = document.createElement('button');
-    button.innerText='Another bad joke?';
-    button.id = 'another-joke-button';
-    button.className = 'custom-button';
-    // STILL HAVE TO FINISH THIS
-}
-
-function joke_appears() {
-    let joke = get_random_joke();
-    const box = document.createElement('div');
-    box.className = 'joke-box'
-    box.innerText = joke;
-    document.body.appendChild(box);
-    another_joke_button();
-}
-
-function get_random_joke() {
-    if (jokes.length === 0) { 
-        console.error("No jokes available. Make sure the CSV file is loaded");
-        return "No jokes available";
-    }
-    const randomIndex = Math.floor(Math.random() * jokes.length); 
-    return jokes[randomIndex];
-}
+readCSV();
+setTimeout(() => {
+    console.log("Random Joke:", get_random_joke());
+}, 1000);
 
 function readCSV() {
     fetch('./shortjokes.csv')
@@ -75,7 +32,64 @@ function parseCSV(csvContent) {
     console.log("Jokes loaded:", jokes);
 }
 
-readCSV();
-setTimeout(() => {
-    console.log("Random Joke:", get_random_joke());
-}, 1000);
+function button_was_clicked() {
+    button_click_status = true;
+    const button = document.getElementById('button');
+    button.remove();
+    h1.remove();
+    joke_appears();
+}
+
+function button_was_dragged() {
+    if (!button_click_status) {
+        const button = document.getElementById('button');
+        button.innerText = "Do you wanna hear a joke?";
+    } 
+}
+
+function reset_button() {
+    if (!button_click_status ) {
+        const button = document.getElementById('button');
+        button.innerText = "Don't be shy";
+        button.style.background = "";
+    }
+}
+
+function another_joke_button() {
+    let button = document.getElementById('another-joke-button');
+    if (!button) {
+        button = document.createElement('button');
+        button.innerText = 'Do you want another bad joke?';
+        button.id = 'another-joke-button';
+        button.className = 'another-joke-button';
+        button.addEventListener('click', () => {
+            console.log('Another joke button clicked');
+            if (currentJokeBox) {
+                currentJokeBox.remove();
+            }
+            joke_appears();
+        });
+        document.body.appendChild(button);
+    }
+}
+
+function joke_appears() {
+    let joke = get_random_joke();
+    const box = document.createElement('div');
+    box.className = 'joke-box';
+    box.innerText = joke;
+    document.body.appendChild(box);
+
+    currentJokeBox = box;
+
+    another_joke_button();
+}
+
+function get_random_joke() {
+    if (jokes.length === 0) { 
+        console.error("No jokes available. Make sure the CSV file is loaded");
+        return "No jokes available";
+    }
+    const randomIndex = Math.floor(Math.random() * jokes.length); 
+    return jokes[randomIndex];
+}
